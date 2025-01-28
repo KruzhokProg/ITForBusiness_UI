e73cf66185a9565647ae3c8b5428b740947b70ae
Nikolay Irtuganov <vongu3@gmail.com>
2025-01-28 17:39:11 +0300

    HW + Image

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,23 +16,29 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    Box()
+//    ImageDemo()
+    ListDemo()
 }
 
 fun main() {
     application(
         content = {
             Window(
+                state = WindowState(width = 600.dp, height = 600.dp),
+                resizable = false,
+                title = "Московские мастера 2025",
                 onCloseRequest = ::exitApplication,
                 content = {
                     App()
@@ -39,6 +46,178 @@ fun main() {
             )
         }
     )
+}
+
+@Composable
+fun ListDemo() {
+    val names = listOf("Коля", "Саша", "Миша")
+    Column {
+        names.forEach { name ->
+            Text(
+                text = name
+            )
+        }
+    }
+}
+
+@Composable
+fun ImageDemo() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Box(
+            modifier = Modifier.align(Alignment.Center),
+        ) {
+            Image(
+                painter = painterResource("drawable/kermit.jpg"),
+                contentDescription = null
+            )
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black)
+                        )
+                    )
+            )
+            IconButton(
+                onClick = {},
+                modifier = Modifier.align(Alignment.BottomEnd),
+                content = {
+                    Icon(
+                        imageVector = Icons.Default.Email,
+                        contentDescription = null,
+                        tint = fuksia
+                    )
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun HW() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(2f),
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .background(
+                        brush = Brush.horizontalGradient(gradientColors)
+                    )
+            )
+            Column(
+                modifier = Modifier.weight(2f)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                        .background(fuksia)
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "Hello",
+                        fontSize = 50.sp
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .background(chocolate)
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "Hello",
+                        fontSize = 50.sp
+                    )
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .background(
+                        color = Color.Red
+                    )
+            )
+
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f),
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .background(Color.Green)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .background(Color.Cyan)
+            )
+        }
+    }
+//    Column() {
+//        Row(
+//            modifier = Modifier.height(100.dp)
+//        ) {
+//            Box(
+//                modifier = Modifier
+//                    .height(100.dp)
+//                    .background(
+//                        brush = Brush.horizontalGradient(gradientColors)
+//                    )
+//            )
+//            Column(
+//                modifier = Modifier
+//                    .height(100.dp)
+//            ) {
+//                Box(
+//                    modifier = Modifier
+//                        .height(100.dp)
+//                        .background(Color.Red)
+//                )
+//                Box(
+//                    modifier = Modifier
+//                        .height(100.dp)
+//                        .background(Color.Green)
+//                )
+//            }
+//            Box(
+//                modifier = Modifier
+//                    .height(100.dp)
+//                    .background(Color.Cyan)
+//            )
+//        }
+//        Row(
+//            modifier = Modifier.height(100.dp)
+//        ) {
+//            Box(
+//                modifier = Modifier
+//                    .height(100.dp)
+//                    .background(Color.Magenta)
+//            )
+//            Box(
+//                modifier = Modifier
+//                    .height(100.dp)
+//                    .background(Color.Blue)
+//            )
+//        }
+//    }
 }
 
 @Composable
@@ -77,7 +256,7 @@ private fun Box() {
 
 @Composable
 private fun TextDemo() {
-    Column (
+    Column(
 //        modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
         modifier = Modifier
             .fillMaxSize()
